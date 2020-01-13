ef82fe0c92c4dc8a0ac4e4e7d7ad24937a573968
Connor Dear <connordear@gmail.com>
2020-01-13 15:28:55 -0500

    udpated with some data formatting (not implemented yet)

--- Backend/Frontend/src/app/semester/scheduler/calendar/calendar.component.ts
+++ Backend/Frontend/src/app/semester/scheduler/calendar/calendar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../../../model/Course';
+// import { currentlyShowing } from './currentlyShowing';
 
 @Component({
   selector: 'app-calendar',
@@ -15,7 +17,34 @@ export class CalendarComponent implements OnInit {
     {name: "Friday"}
   ];
 
+  // courseLists: Course[][] = this.buildCourseLists();
 
+  // buildCourseLists(): Course[][] {
+  //   let res: Course[][] = [[], [], [], [], []];
+  //   for (const course of currentlyShowing) {
+  //     for (const aDay of course.days) {
+  //       res[aDay].push(new Course(
+  //         course.number,
+  //         course.dept,
+  //         course.instructor,
+  //         course.code,
+  //         course.starting_block,
+  //         course.colour,
+  //         course.name,
+  //         course.section,
+  //         course.days,
+  //         course.year,
+  //         course.session,
+  //         course.type,
+  //         course.labSection
+  //       ));
+  //     }
+  //     return res;
+  //   }
+
+    
+
+  // }
 
   constructor() { }
 
--- Backend/Frontend/src/app/semester/scheduler/calendar/day/dayConfig.js
+++ Backend/Frontend/src/app/semester/scheduler/calendar/day/dayConfig.js
@@ -1,3 +1,3 @@
 export const dayConfig = {
-  numPeriodsPerDay : 28
+  numPeriodsPerDay : 27
 };
