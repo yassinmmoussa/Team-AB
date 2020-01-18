12176d4e7c72a4a44a7583aa2159da3136c1c393
Connor Dear <connordear@gmail.com>
2020-01-18 11:09:14 -0500

    holy shit its actually kinda working

--- Backend/Frontend/src/app/semester/scheduler/calendar/day/block/block.component.ts
+++ Backend/Frontend/src/app/semester/scheduler/calendar/day/block/block.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Course } from '../../../../../model/Course';
+import { blockHeight } from '../dayConfig.js';
 
 @Component({
   selector: 'app-block',
@@ -9,10 +10,10 @@ import { Course } from '../../../../../model/Course';
 export class BlockComponent implements OnInit {
 
   @Input() courses: Course[];
-
   constructor() { }
 
   ngOnInit() {
+    console.log('blockheight:' + this.blockHeight);
   }
 
 }
