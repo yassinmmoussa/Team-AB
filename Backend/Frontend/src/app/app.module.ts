41c1a6356703271c561fb176194f0cb6e4224b29
JeremyWinkler <jeremy.winkler1@gmail.com>
2020-01-19 17:52:45 -0500

    remove nodemodules

--- Backend/Frontend/src/app/app.module.ts
+++ Backend/Frontend/src/app/app.module.ts
@@ -22,8 +22,6 @@ import { CourseListComponent } from './semester/filter/course-list/course-list.c
 import { ConfigBarComponent } from './semester/scheduler/config-bar/config-bar.component';
 import { CalendarComponent } from './semester/scheduler/calendar/calendar.component';
 import { DayComponent } from './semester/scheduler/calendar/day/day.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
