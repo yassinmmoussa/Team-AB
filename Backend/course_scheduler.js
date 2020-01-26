5309764962ef7741a84d4dd1a95427327bea49ac
Connor Ahearn <connor.ahearn305@gmail.com>
2020-01-26 16:30:41 -0500

    Built up basic algorithm for building a request to CourseScheduler

--- Backend/course_scheduler.js
+++ Backend/course_scheduler.js
@@ -1,6 +1,35 @@
+// MVP Request solutions count
+const solutions = 1;
 
+function schedule() {
 
+    // Request sent to CourseScheduler flask server in order to receive solutions
+    let request = {};
 
+    // Set # of solutions we would like to receive
+    request.solutions = solutions;
+
+    // Collection of curricula to be sceduled in the current run
+    let curricula;
+    
+    //TODO: Database call filling curricula with objects from the database
+
+    curricula.foreach(curriculum, function() {
+
+        //TODO: Get all the courses of a given curriculum
+        let courses;
+
+        courses.foreach(course, function() {
+
+            //TODO: Make a database call for this specific course to see how many periods it requires
+        });
+
+    });
+
+    //TODO: Build constraints JSON object, ideally partially built by frontend
+
+    //TODO: Build and finalize final JSON object
+}
 
 
 module.exports = function() {console.log("Consider your courses.... scheduled")}
