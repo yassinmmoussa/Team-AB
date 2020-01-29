aa158ecddce9765022553a3c2d13785e0b8eb41b
Connor Dear <connordear@gmail.com>
2020-01-29 15:03:42 -0500

    figured out key hiding stuff

--- Backend/top_secret/database.js
+++ Backend/top_secret/database.js
@@ -3,7 +3,9 @@ const session = require('express-session');
 
 const {FirestoreStore} = require('@google-cloud/connect-firestore');
 
+// ------------  DO NOT MODIFY THIS FILE ------------------//
 module.exports = new Firestore({
     projectId: 'spare-ab',
-    keyFilename: './top_secret/keys/spare-ab-YOUR_KEY_NAME_HERE.json', // Put your key here
+    keyFilename: './top_secret/keys/spare-ab-key.json', // Rename your key to match this
   });
+  // ------------  DO NOT MODIFY THIS FILE ------------------//
