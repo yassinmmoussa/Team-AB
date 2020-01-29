5effc1e25221b586223dcf42c9fa98feae03d7e5
Connor Dear <connordear@gmail.com>
2020-01-29 14:47:47 -0500

    deleted top_secret tracking

--- Backend/top_secret/database.js
+++
@@ -1,9 +0,0 @@
-const {Firestore} = require('@google-cloud/firestore');
-const session = require('express-session');
-
-const {FirestoreStore} = require('@google-cloud/connect-firestore');
-
-module.exports = new Firestore({
-    projectId: 'spare-ab',
-    keyFilename: './top_secret/spare-ab-7cdd504af0c3.json', // This key is no longer valid
-  });
