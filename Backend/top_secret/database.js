8cdc64f57f1b92eaed7b132bab6909312addcdc0
Nhi Vuong <nhivuong@Nhis-MacBook-Pro.local>
2020-02-02 10:23:00 -0500

    auto authenticate db under spare-ab

--- Backend/top_secret/database.js
+++ Backend/top_secret/database.js
@@ -4,8 +4,23 @@ const session = require('express-session');
 const {FirestoreStore} = require('@google-cloud/connect-firestore');
 
 // ------------  DO NOT MODIFY THIS FILE ------------------//
-module.exports = new Firestore({
+
+// Old Ver
+/*module.exports = new Firestore({
     projectId: 'spare-ab',
     keyFilename: './top_secret/keys/spare-ab-key.json', // Rename your key to match this
-  });
-  // ------------  DO NOT MODIFY THIS FILE ------------------//
+  });*/
+  
+// New Ver for auto-authenticate
+  if (process.env.DEV === "true") {
+    module.exports = new Firestore({
+        projectId: 'spare-ab',
+        keyFilename: './top_secret/keys/spare-ab-key.json'
+    });
+} else {
+    module.exports = new Firestore({
+        projectId: 'spare-ab'
+    });
+}
+
+// ------------  DO NOT MODIFY THIS FILE ------------------//
