84470545eeb46c3b371ea7780a2ddb69812b87ff
Nhi Vuong <nhivuong@Nhis-MacBook-Pro.local>
2020-02-09 11:45:21 -0500

    I finalized the request body

--- Backend/course_scheduler.js
+++ Backend/course_scheduler.js
@@ -15,21 +15,67 @@ function schedule(year, session, next) {
 
     database.scheduler_curricula(year, session, function(curricula) {
 
-        console.log("Scheduler: " + curricula);
+        //console.log("Scheduler: " ,curricula);
 
         curricula.forEach(curriculum => {
+
+            console.log("1 curriculum: " , curriculum.courses);
             
-            let requestCurriculum = {};
+            //let requestCurriculum = {}; //cponnor
 
             // This needs to change to a real course ID
-            requestCurriculum.curriculum_id = "id";
+            //requestCurriculum.curriculum_id = "id"; //connor
 
-            request.courses = curriculum.courses;
+            request.curricula.curriculum_id = curriculum.id;   //nhi
+            //request.courses = curriculum.courses; //Connor's
+            request.curricula.courses = [];                   // Nhi's
+
+            
+            //request.courses.forEach( cID => {
+            curriculum.courses.forEach( cID => {
+                //let collectionName = cID._path.segments[0]; //not needed
+                let courseID = cID._path.segments[1];
+
+                database.scheduler_course(courseID, function(duration) {
+                    let requestCourseSet = {};
+                    requestCourseSet.courseID = courseID;
+                    requestCourseSet.n_periods=duration;
+
+                    request.curricula.courses.push(requestCourseSet);
+
+                    console.log(request);
+                });
+
+            });
 
         });
 
-
-        request.constraints = [];
+        request.constraints = [
+        {
+            "course_id": "hFUhTu8WIEeQEQ3i",
+            "day": 2,
+            "intervals": [
+              {"start": 0,"end": 4},
+              {"start": 6,"end": 9}
+            ]
+        },{
+            "course_id": "hFUhTu8WIEeQEQ3i",
+            "day": 4,
+            "intervals": [
+              {"start": 4,"end": 9
+              }
+            ]
+          },{
+            "course_id": "jWtVT6TsTjz0lFQb",
+            "day": 3,
+            "intervals": [
+              {"start": 10,"end": 14},
+              {"start": 16,"end": 19
+              }
+            ]
+          }
+        ]
+        //request.constraints = [];
 
         next(request);
 
