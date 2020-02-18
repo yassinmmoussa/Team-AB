b150eb559af5955881902e4e05628f6e1db3b8db
Connor Dear <connordear@gmail.com>
2020-02-18 10:17:35 -0500

    added handleError to data service

--- Backend/Frontend/src/app/services/data.service.ts
+++ Backend/Frontend/src/app/services/data.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Course } from '../model/Course';
 import { Curricula } from '../model/Curricula';
 import { data } from '../semester/scheduler/calendar/data';
-import { of } from 'rxjs';
+import { of, Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -32,20 +33,45 @@ export class DataService {
   }
 
   runOptimizer(courses: Course[], curricula: Curricula[]) {
-    // return this.http.get('/api/schedule/runOptimizer', {
+    // console.log(`${courses.map(course => course.toJSON())}`);
+    // console.log(`${curricula.map(curr => curr.toJSON())}`);
+    const optimizerURL = '/api/schedule/runOptimizer';
+    const courseData = `${courses.map(course => course.toJSON())}`;
+    const curriculaData = `${curricula.map(curr => curr.toJSON())}`;
+
+    return this.http.post(optimizerURL, {
+      params: {
+        courses: courseData,
+        curricula: curriculaData
+      }
+    }).pipe(
+      catchError(this.handleError)
+    );
+
+    // Old Get request... doesn't work for large payloads
+    // return this.http.get('/bad_test', {
     //   params: {
     //     courses: `${courses.map(course => course.toJSON())}`,
     //     curricula: `${curricula.map(curr => curr.toJSON())}`
     //   }
     // }
-    return this.http.get('/bad_test', {
-      params: {
-        courses: `${courses.map(course => course.toJSON())}`,
-        curricula: `${curricula.map(curr => curr.toJSON())}`
-      }
-    }
-    );
+    // );
+
   }
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
+  }
 
 }
